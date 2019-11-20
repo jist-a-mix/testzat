164401d4d873af76edd4198283ad244d035b9467
mik <autoclik974@gmail.com>
2019-11-20 15:49:34 +0100

    set routes and namespaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	 # Api definition
   namespace :api, defaults: { format: :json }   do
-  	namespace :v1 do
+  	namespace :v1 do 
     # We are going to list our resources here
 end
   end
