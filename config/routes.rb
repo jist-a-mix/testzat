6a06f7be2d6af8d2968e271e7c71b93673b2abe0
mik <autoclik974@gmail.com>
2019-11-22 14:55:06 +0100

    modeling the product model along with user associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get 'tokens/create'
-    end
-  end
-	 # Api definition
+ 
   namespace :api, defaults: { format: :json }   do
   	namespace :v1 do 
   		resources :users, only: %i[show create update destroy]
   		resources :tokens, only: [:create]
+  		resources :products, only: %i[show index]
     # We are going to list our resources here
 end
   end
