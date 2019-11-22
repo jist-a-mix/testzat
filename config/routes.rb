0ff844cb2ee978d4144c2761a55c8c68fc5fc573
mik <autoclik974@gmail.com>
2019-11-22 15:14:39 +0100

    Adds the products create, update and destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   	namespace :v1 do 
   		resources :users, only: %i[show create update destroy]
   		resources :tokens, only: [:create]
-  		resources :products, only: %i[show index]
-    # We are going to list our resources here
+  		resources :products    # We are going to list our resources here
 end
   end
 end
